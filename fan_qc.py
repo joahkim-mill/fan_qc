a9a1297fc7b4b61e7dce8bc69be89b67f7dc0bf7
joah kim <joah.kim@chewielabs.com>
2024-01-11 14:45:03 -0800

    first commit

---
+++ fan_qc.py
@@ -0,0 +1,47 @@
+import pandas as pd 
+import plotly.graph_objects as go 
+from scipy.signal import find_peaks
+import streamlit as st
+
+"""
+Once noise recordings from REW have been exported as .txt files, can be read in here and extrapolated for 
+peak frequencies above a specified dB threshold. The peaks and corresponding dB/Hz values will be printed.
+Can optionally plot the data as well.
+"""
+
+filename = "Dec 22 peak - 4"
+data = pd.read_csv(f"{filename}.txt", sep=" ", header=None, skiprows=14)
+data.columns = ["Freq(Hz)", "SPL(dB)"] 
+# print(data)
+# data.to_csv(f"C:/fan_qc/{filename}.csv")  ## to write the csv file --> make sure the location is correct
+
+dB_threshold = 45  # dB threshold at which we want to find which frequencies are peaks
+peaks, _ = find_peaks(data["SPL(dB)"], height=dB_threshold) # find the peaks in the dB
+
+# print("        Peaks     ")
+# for p in peaks:
+#     print(f"{data["SPL(dB)"][p]} dB      {data["Freq(Hz)"][p]} Hz")
+
+
+ ## plot data ##
+
+fig = go.Figure()
+
+fig.add_trace(go.Scatter(x=data["Freq(Hz)"], y=data["SPL(dB)"], name="Noise Recording"))
+fig.add_trace(go.Scatter(mode='markers', x=data["Freq(Hz)"][peaks], y=data["SPL(dB)"][peaks], name="Peak Frequencies", marker=dict(color='red')))
+fig.update_layout(title="Fan Noise Recording", 
+                  xaxis_title="Frequency Spectrum [Hz]", 
+                  yaxis_title="SPL[dB]",
+                  height=600,
+                  width=800
+                  )
+fig.update_xaxes(title_font_color="black")
+fig.update_yaxes(title_font_color="black")
+# fig.show()
+st.plotly_chart(fig)
+
+# print out the peak data points
+st.text("        Peaks        ")
+for p in peaks:
+    st.text(f"{data["SPL(dB)"][p]} dB      {data["Freq(Hz)"][p]} Hz \n")
+
