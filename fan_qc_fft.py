9b35b9bcf002c97ff07dd98d5076890bacf32c4e
joah kim <joah.kim@chewielabs.com>
2024-01-16 12:56:10 -0800

    utilize streamlit

--- fan_qc_fft.py
+++ fan_qc_fft.py
@@ -1,10 +1,10 @@
 import pandas as pd 
 import numpy as np 
 from scipy.fft import fft, fftfreq
-# import streamlit as st 
+import streamlit as st 
 import plotly.graph_objects as go 
 
-i=1
+i=5
 url = f"https://github.com/joahkim-mill/fan_qc/raw/main/prebaked_delta/Fan%20{i}.txt"
 data = pd.read_csv(url, sep=" ", header=None, skiprows=14, engine='python')
 data.columns = ["Freq(Hz)", "SPL(dB)", "Phase(degrees)"] 
@@ -24,5 +24,13 @@ y = 2.0/N * abs(yf[0:N//2])
 
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=xf, y=y[:,1], line_shape='linear')) 
-fig.show()
+
+fig.update_layout(title=f"Pre-Baked Fan #{i}", 
+                  xaxis_title="Frequency [Hz]", 
+                #   yaxis_title="SPL [dB]",
+                  height=600,
+                  width=800)
+
+st.title(f"Fan FFT Analysis")
+st.plotly_chart(fig)
 
