e4bf66dab26ee74946501d66ea4b3819e1357d87
joah kim <joah.kim@chewielabs.com>
2024-02-07 17:50:42 -0800

    start to compare stats

--- vibration_testing/compare_accel.py
+++ vibration_testing/compare_accel.py
@@ -2,8 +2,12 @@ import pandas as pd
 import numpy as np 
 from plotly import graph_objects as go 
 from plotly.subplots import make_subplots
+from scipy.signal import find_peaks 
 
-## ------- GOOD FAN DATA -------- ##
+
+#region -- good fan data
+#### --------- GOOD FAN DATA --------- ####
+# -- GOOD 7 -- #
 # read in csv to access data 
 good7 = pd.read_csv("./accel_data/good_7.csv")
 
@@ -12,8 +16,46 @@ t_g7 = np.asarray(good7["time"])
 ax_g7 = np.asarray(good7["x_accel"])
 ay_g7 = np.asarray(good7["y_accel"])
 az_g7 = np.asarray(good7["z_accel"])
+a_g7 = np.hstack((ax_g7.reshape((-1,1)), ay_g7.reshape((-1,1)), az_g7.reshape((-1,1))))
 
-## ------- BAD FAN DATA -------- ##
+# -- GOOD 8 -- #
+# read in csv to access data 
+good8 = pd.read_csv("./accel_data/good_8.csv")
+
+# make them into columns
+t_g8 = np.asarray(good8["time"])
+ax_g8 = np.asarray(good8["x_accel"])
+ay_g8 = np.asarray(good8["y_accel"])
+az_g8 = np.asarray(good8["z_accel"])
+a_g8 = np.hstack((ax_g8.reshape((-1,1)), ay_g8.reshape((-1,1)), az_g8.reshape((-1,1))))
+
+# -- GOOD 9 -- #
+# read in csv to access data 
+good9 = pd.read_csv("./accel_data/good_9.csv")
+
+# make them into columns
+t_g9 = np.asarray(good9["time"])
+ax_g9 = np.asarray(good9["x_accel"])
+ay_g9 = np.asarray(good9["y_accel"])
+az_g9 = np.asarray(good9["z_accel"])
+a_g9 = np.hstack((ax_g9.reshape((-1,1)), ay_g9.reshape((-1,1)), az_g9.reshape((-1,1))))
+
+# -- GOOD 10 -- #
+# read in csv to access data 
+good10= pd.read_csv("./accel_data/good_10.csv")
+
+# make them into columns
+t_g10 = np.asarray(good10["time"])
+ax_g10 = np.asarray(good10["x_accel"])
+ay_g10 = np.asarray(good10["y_accel"])
+az_g10 = np.asarray(good10["z_accel"])
+a_g10 = np.hstack((ax_g10.reshape((-1,1)), ay_g10.reshape((-1,1)), az_g10.reshape((-1,1))))
+
+#endregion -- good fan data
+
+#region -- bad fan data
+#### --------- BAD FAN DATA --------- ####
+# -- BAD 1 -- #
 bad1 = pd.read_csv("./accel_data/bad_1.csv")
 
 # make them into columns
@@ -21,8 +63,9 @@ t_b1 = np.asarray(bad1["time"])
 ax_b1 = np.asarray(bad1["x_accel"])
 ay_b1 = np.asarray(bad1["y_accel"])
 az_b1 = np.asarray(bad1["z_accel"])
+a_b1 = np.hstack((ax_b1.reshape((-1,1)), ay_b1.reshape((-1,1)), az_b1.reshape((-1,1))))
 
-## ------- BAD FAN DATA -------- ##
+# -- BAD 2 -- #
 bad2 = pd.read_csv("./accel_data/bad_2.csv")
 
 # make them into columns
@@ -30,8 +73,9 @@ t_b2 = np.asarray(bad2["time"])
 ax_b2 = np.asarray(bad2["x_accel"])
 ay_b2 = np.asarray(bad2["y_accel"])
 az_b2 = np.asarray(bad2["z_accel"])
+a_b2 = np.hstack((ax_b2.reshape((-1,1)), ay_b2.reshape((-1,1)), az_b2.reshape((-1,1))))
 
-## ------- BAD FAN DATA -------- ##
+# -- BAD 3 -- #
 bad3 = pd.read_csv("./accel_data/bad_3.csv")
 
 # make them into columns
@@ -39,8 +83,14 @@ t_b3 = np.asarray(bad3["time"])
 ax_b3 = np.asarray(bad3["x_accel"])
 ay_b3 = np.asarray(bad3["y_accel"])
 az_b3 = np.asarray(bad3["z_accel"])
+a_b3 = np.hstack((ax_b3.reshape((-1,1)), ay_b3.reshape((-1,1)), az_b3.reshape((-1,1))))
 
-"""
+#endregion -- bad fan data
+
+data_g = [a_g7, a_g8, a_g9]  ## NOTE : exluding good fan 10 bc it's not actually that good --> i think it should be labeled bad
+data_b = [a_b1, a_b2, a_b3] 
+
+""" #region -- plots
 #### --------- CREATE PLOTS --------- ####
 fig = make_subplots(rows=3, cols=1,
                     subplot_titles=("X", "Y", "Z"), 
@@ -108,12 +158,46 @@ fig.show()
 
 #### ------ END PLOTS -------- ####
 
-"""
+""" #endregion -- plots
+
+#region -- calc stats
 
 #### ------ CALCULATE STATS ------- ####
 
-print(good.mean(axis=0))
-print(good)
-print(bad1.mean(axis=0))
-print(bad2.mean(axis=0))
-print(bad3.mean(axis=0))
+# 1. take averages of raw data : means are all pretty similar ---> std dev may be promising ? 
+# print("good fans")
+# for g in data_g:
+#     print(f"avg : ", np.mean(g, axis=0))
+#     print(f"std dev : ", np.std(g, axis=0))
+
+# print("bad fans")
+# for b in data_b:
+#     print(f"avg : ", np.mean(b, axis=0))
+#     print(f"std dev : ", np.std(b, axis=0))
+
+
+# 2. max [absolute] amplitude --> doens't seem relevant ... except the y accel for bad fans are pretty bad
+# print("good fans")
+# for g in data_g:
+#     # absolute = np.abs(g)
+#     print("max amplitude : ", np.amax(g, axis=0))
+
+# print("bad fans")
+# for b in data_b:
+#     # absolute = np.abs(b)
+#     print("max amplitude : ", np.amax(b, axis=0))
+
+ 
+# 3. use find peaks ?? FINISH HERE !
+fig = go.Figure()
+t_g = [t_g7, t_g8, t_g9]
+t_b = [t_b1, t_b2, t_b3]
+for i in range(3):
+    peaks, _ = find_peaks(data_g[i])
+    fig.add_trace(x=t_g[i], y=data_g[i])
+    fig.add_trace(go.Scatter(x=t_g[i][peaks], y=data_g[i][peaks], mode='markers'))
+
+# 4.  maybe determine at what theshold the amplitude hits a percentile ?
+
+
+#endregion -- calc stats
