0767ad0993837602eb5284bf60b43ef3a89a752d
joah kim <joah.kim@chewielabs.com>
2024-02-21 18:20:00 -0800

    continue nn

--- vibration_testing/classifier.py
+++ vibration_testing/classifier.py
@@ -16,16 +16,22 @@ class PiezoModel(nn.Module):
     def forward(self, x):
         return self.layer_2(self.layer_1(x))  # layer 1 --> layer 2
 
-"""  # create tensors and save them to be loaded in 
+def accuracy_fn(y_true, y_pred):
+    correct = torch.eq(y_true, y_pred).sum().item() 
+    accuracy = (correct / len(y_pred)) * 100 
+    return accuracy 
+
+"""
+#region create tensors and save them to be loaded in 
 # create numpy array of all the data to be used for testing/training sets
-good = [1, 7, 8, 9, 10]
+good = range(1,68)
 bad = range(1,14)
 
 numFans = len(good) + len(bad)
 X = np.zeros((numFans, 8191))  
 y = np.zeros((numFans,))  #   0 --> bad fan  ,  1 --> good fan
 for b in bad:
-    filepath = f"./piezo_audacity_data/bad_{b}.txt"
+    filepath = f"./piezo_audacity_data/bad/bad_{b}.txt"
     data = pd.read_csv(filepath, sep='\t')
     # freq = data["Frequency (Hz)"]
     db = np.asarray(data["Level (dB)"]).ravel() 
@@ -33,7 +39,7 @@ for b in bad:
     y[b-1] = 0
 
 for g in range(len(good)):
-    filepath = f"./piezo_audacity_data/good_{good[g]}.txt"
+    filepath = f"./piezo_audacity_data/good/good_{good[g]}.txt"
     data = pd.read_csv(filepath, sep='\t')
     db = np.asarray(data["Level (dB)"]).ravel() 
     X[len(bad) + g, :] = db 
@@ -42,10 +48,12 @@ for g in range(len(good)):
 X = torch.from_numpy(X).type(torch.float)
 y = torch.from_numpy(y).type(torch.float) 
 
-# torch.save(X, 'X_tensor.pt') 
-# torch.save(y, 'y_tensor.pt')
+torch.save(X, 'X_tensor.pt') 
+torch.save(y, 'y_tensor.pt')
 
+#endregion create tensors
 """
+
 X = torch.load('X_tensor.pt')
 y = torch.load('y_tensor.pt')
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2) #20% test, 80% train
@@ -57,6 +65,16 @@ model = PiezoModel().to(device)
 untrained_predictions = model(X_test.to(device))
 # print(f"Length of predictions: {len(untrained_predictions)}, Shape: {untrained_predictions.shape}")
 # print(f"Length of test samples: {len(y_test)}, Shape: {y_test.shape}")
+
+# loss function
+loss_fn = nn.BCEWithLogitsLoss()
+
+# optimizer
+optimizer = torch.optim.SGD(params=model.parameters(), lr=0.1)
+
+y_logits = model(X_test.to(device))  # predictions in raw output form
+y_pred_probs = torch.sigmoid(y_logits)  # predictions in probability form
+y_preds = torch.round(y_pred_probs)  # round to either 0 or 1 
 
 
 
