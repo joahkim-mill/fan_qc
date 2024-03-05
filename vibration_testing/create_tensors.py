ff90f66a28386ae626346fe1940b2baedb87363d
joah kim <joah.kim@chewielabs.com>
2024-03-04 17:57:31 -0800

    use spring data for avg/std dev

--- vibration_testing/create_tensors.py
+++ vibration_testing/create_tensors.py
@@ -2,34 +2,68 @@ import numpy as np
 import pandas as pd
 import torch
 
+#region create tensor of good fan data and save it to be loaded in 
+# create numpy array of all the good fan data to be used 
+good = list(range(1,67))
+good.pop(61) # don't have data for fan 62
 
-#region create tensors and save them to be loaded in 
-# create numpy array of all the data to be used for testing/training sets
-good = range(1,68)
-bad = range(1,17)
+# print(good)
 
-numFans = len(good) + len(bad)
-X = np.zeros((numFans, 8191))  
-y = np.zeros((numFans,))  #   0 --> bad fan  ,  1 --> good fan
-for b in bad:
-    filepath = f"./piezo_audacity_data/bad/bad_{b}.txt"
+numFans = len(good)
+spring_good = np.zeros((numFans, 8191))  
+
+# for b in bad:
+#     filepath = f"./piezo_audacity_data/bad/bad_{b}.txt"
+#     data = pd.read_csv(filepath, sep='\t')
+#     # freq = data["Frequency (Hz)"]
+#     db = np.asarray(data["Level (dB)"]).ravel() 
+#     X[b-1,:] = db  # set each row to be a set of fan dB datapoints across frequecy spectrum
+#     y[b-1] = 0
+count = 0
+for g in good:
+    filepath = f"./piezo_audacity_data/spring_good/{g}.txt"
     data = pd.read_csv(filepath, sep='\t')
-    # freq = data["Frequency (Hz)"]
     db = np.asarray(data["Level (dB)"]).ravel() 
-    X[b-1,:] = db  # set each row to be a set of fan dB datapoints across frequecy spectrum
-    y[b-1] = 0
+    spring_good[count, :] = db 
 
-for g in range(len(good)):
-    filepath = f"./piezo_audacity_data/good/good_{good[g]}.txt"
-    data = pd.read_csv(filepath, sep='\t')
-    db = np.asarray(data["Level (dB)"]).ravel() 
-    X[len(bad) + g, :] = db 
-    y[len(bad) + g] = 1
+spring_good = torch.from_numpy(spring_good).type(torch.float)
+# y = torch.from_numpy(y).type(torch.float) 
 
-X = torch.from_numpy(X).type(torch.float)
-y = torch.from_numpy(y).type(torch.float) 
-
-torch.save(X, 'X_tensor.pt') 
-torch.save(y, 'y_tensor.pt')
+torch.save(spring_good, 'spring_good.pt') 
+# torch.save(y, 'y_tensor.pt')
 
 #endregion create tensors
+
+
+
+# #region create tensors and save them to be loaded in 
+# # create numpy array of all the data to be used for testing/training sets
+# good = range(1,68)
+# bad = range(1,17)
+
+# numFans = len(good) + len(bad)
+# X = np.zeros((numFans, 8191))  
+# y = np.zeros((numFans,))  #   0 --> bad fan  ,  1 --> good fan
+# for b in bad:
+#     filepath = f"./piezo_audacity_data/bad/bad_{b}.txt"
+#     data = pd.read_csv(filepath, sep='\t')
+#     # freq = data["Frequency (Hz)"]
+#     db = np.asarray(data["Level (dB)"]).ravel() 
+#     X[b-1,:] = db  # set each row to be a set of fan dB datapoints across frequecy spectrum
+#     y[b-1] = 0
+
+# for g in range(len(good)):
+#     filepath = f"./piezo_audacity_data/good/good_{good[g]}.txt"
+#     data = pd.read_csv(filepath, sep='\t')
+#     db = np.asarray(data["Level (dB)"]).ravel() 
+#     X[len(bad) + g, :] = db 
+#     y[len(bad) + g] = 1
+
+# X = torch.from_numpy(X).type(torch.float)
+# y = torch.from_numpy(y).type(torch.float) 
+
+# torch.save(X, 'X_tensor.pt') 
+# torch.save(y, 'y_tensor.pt')
+
+# #endregion create tensors
+
--- vibration_testing/spectrum_analysis.py
+++ vibration_testing/spectrum_analysis.py
@@ -6,151 +6,120 @@ import streamlit as st
 from scipy.signal import find_peaks, find_peaks_cwt
 import torch 
 
-# good = range(1,10)
-# bad = range(1,14) 
-# good = [1]
-# bad = [1]
 
+#region compute average of good fans
+X = torch.load('spring_good.pt')  
+good = X.numpy()
+freq = torch.load('frequency.pt')
+freq = freq.numpy()
 
-# #region plotting good and bad on separate subplots
-# fig = make_subplots(rows=2, cols=1, vertical_spacing=0.2)
-# for g in good:
-#     filepath = f"./piezo_audacity_data/good_{g}.txt"
-#     good_data = pd.read_csv(filepath, sep='\t')
-#     print(good_data)
-#     freq = good_data["Frequency (Hz)"]
-#     db = good_data["Level (dB)"]
-#     fig.add_trace(go.Scatter(x=freq, y=db, name=f"Good {g}", 
-#                             #  legendgroup='1'
-#                              ), 
-#                              row=1, col=1)
+avg = np.mean(good, axis=0)
+std = np.std(good, axis=0)
 
-# for b in bad:
-#     filepath = f"./piezo_audacity_data/bad_{b}.txt"
-#     bad_data = pd.read_csv(filepath, sep='\t')
+# # create dataframe to save the stats
+# df = pd.DataFrame()
+# df["avg"] = avg 
+# df["std dev"] = std
 
-#     freq = bad_data["Frequency (Hz)"]
-#     db = bad_data["Level (dB)"]
-#     fig.add_trace(go.Scatter(x=freq, y=db, name=f"Bad {b}", 
-#                             #  legendgroup='2'
-#                              ), 
-#                              row=2, col=1)
+# df.to_csv("model")
 
-# fig.update_layout(title="Fan Spectrum Visualization", height=800, width=900,
-#                   xaxis1_title="Frequency [Hz]", yaxis1_title="Level [dB]",
-#                   xaxis2_title="Frequency [Hz]", yaxis2_title="Level [dB]",
-#                 #   legend_tracegroupgap=250
-#                   )
-
-# st.plotly_chart(fig)
-# #endregion subplots
-
-# #region plotting all together on one plot
+# #region streamlit plotting
 # fig = go.Figure()
-# for g in good:
-#     filepath = f"./piezo_audacity_data/good/good_{g}.txt"
-#     good_data = pd.read_csv(filepath, sep='\t')
-#     freq = good_data["Frequency (Hz)"]
-#     db = good_data["Level (dB)"]
-#     fig.add_trace(go.Scatter(x=freq, y=db, name=f"Good {g}", line=dict(width=1.25)))
-# for b in bad:
-#     filepath = f"./piezo_audacity_data/bad/bad_{b}.txt"
-#     bad_data = pd.read_csv(filepath, sep='\t')
-#     freq = bad_data["Frequency (Hz)"]
-#     db = bad_data["Level (dB)"]
-#     fig.add_trace(go.Scatter(x=freq, y=db, name=f"Bad {b}", line=dict(width=1.25)))
-# fig.update_layout(title="Fan Frequency Spectrum", height=800, width=900,
-#                   xaxis_title="Frequency [Hz]", yaxis_title="Level [dB]")
-# st.plotly_chart(fig)
-# #endregion
 
+# # # std_dev_mult = 1.5
+# std_dev_mult = st.number_input("Input a Standard Deviation Multiplier")
+# std_dev = np.concatenate([(avg+ std_dev_mult*std), (avg- std_dev_mult*std)[::-1]])
 
-# #region find peaks of signals
-# fig = go.Figure()
-# db_good= np.zeros((8191,))  # each row will contain level[dB] data across frequencies
-# db_bad = np.zeros((8191,))
+# out_b= np.zeros((bad.shape[0],))
+# out_g = np.zeros((good.shape[0],))
 
+# fig.add_trace(go.Scatter(x=np.concatenate([freq, freq[::-1]]), y=std_dev, name=f"Avg +/- {std_dev_mult} Std Dev", 
+#                          fill='toself',
+#                          line=dict(width=0.5, color='#fad7f1'),
+#                          ))
+# fig.add_trace(go.Scatter(x=freq, y=avg, name='average good fan',
+#                          line=dict(width=0.75, color='#bc37ed')))
 
-# good_peaks_df = pd.DataFrame(index=range(14))  # change these index ranges depending on how many peaks are found !!
-# bad_peaks_df = pd.DataFrame(index=range(15))
-# for g in good:
-#     filepath = f"./piezo_audacity_data/good_{g}.txt"
-#     good_data = pd.read_csv(filepath, sep='\t')
-#     freq = good_data["Frequency (Hz)"]
-#     db = np.asarray(good_data["Level (dB)"]).ravel() 
-#     db_good = np.vstack((db_good, db)) # adding on rows to the array of data
-#     fig.add_trace(go.Scatter(x=freq, y=db, name=f"good {g}", line=dict(width=1)))
-#     # peaks, _ = find_peaks(db, rel_height=10)
-#     peaks = find_peaks_cwt(db, widths=range(60, 200))
-#     # print(f"good peaks num: {peaks.shape}")
+# check_peak = st.checkbox('Use Peaks')
+# #endregion streamlit plotting
+
+# #region inspect the good redo recordings
+# # since this comes after finding the average and standard deviation of the original dataset, we can see how the re-recorded fans do 
+# redo = [10, 22, 24, 27, 37, 52, 64, 65, 7, 47, 49, 51, 14, 48, 53, 61]
+
+# for r in redo: # starts from 0, so fan 1 is good[0] and so on
+#     filepath = f"./piezo_audacity_data/good/good_{r}_redo.txt"
+#     data = pd.read_csv(filepath, sep='\t')
+#     db = np.asarray(data["Level (dB)"]).ravel() 
+#     # replace original good data with the redo 
+#     good[r - 1] = db
+
+# redo_bad = [8, 11, 12, 13, 15]
+
+# for rb in redo_bad:
+#     filepath = f"./piezo_audacity_data/bad/bad_{rb}_redo.txt"
+#     data = pd.read_csv(filepath, sep='\t')
+#     db = np.asarray(data["Level (dB)"]).ravel()
+#     bad[rb - 1] = db
+# #endregion inspect the good redo recordings
     
-#     good_peaks_df[f"good {g}"] = freq[peaks].reset_index(drop=True)
-#     fig.add_trace(go.Scatter(x=freq[peaks], y=db[peaks], name=f"good {g} peaks", mode='markers'))
+# #region find the crossings of avg +/- ()*std dev
+# count = 0
 # for b in bad:
-#     filepath = f"./piezo_audacity_data/bad_{b}.txt"
-#     bad_data = pd.read_csv(filepath, sep='\t')
-#     freq = bad_data["Frequency (Hz)"]
-#     db = np.asarray(bad_data["Level (dB)"]).ravel()
-#     db_bad = np.vstack((db_bad, db)) # adding on rows to the array of data
-#     fig.add_trace(go.Scatter(x=freq, y=db, name=f"bad {b}", line=dict(width=1)))
-#     # peaks, _ = find_peaks(db, rel_height=10)
-#     peaks = find_peaks_cwt(db, widths=range(60,200))
-#     # print(f"bad peaks num: {peaks.shape}")
-  
-#     bad_peaks_df[f"bad {b}"] = freq[peaks].reset_index(drop=True)
-#     fig.add_trace(go.Scatter(x=freq[peaks], y=db[peaks], name=f"bad {b} peaks", mode='markers'))
+#     if check_peak:
+#         # using peaks instead of raw data points 
+#         peaks, _ = find_peaks(b)
+#         out_b[count] = (int)(np.sum((b > (avg + std_dev_mult*std))[peaks]))
+#     else:
+#         # using raw data points
+#         out_b[count] = (int)(np.sum(b > (avg + std_dev_mult*std)))
+    
+#     count += 1
 
-# # get rid of row of zeros
-# db_good = db_good[1:,:]
-# db_bad = db_bad[1:,:]
+#     fig.add_trace(go.Scatter(x=freq, y=b, name=f"bad fan {count}",
+#                              line=dict(width=0.5)))
+    
+# count = 0
+# for g in good: 
+#     if check_peak:
+#         # using peaks instead of raw data points
+#         peaks, _ = find_peaks(g)
+#         out_g[count] = (int)(np.sum((g > (avg + std_dev_mult*std))[peaks]))
 
-# # avg_db_good = np.mean(db_good, axis=0)
-# # avg_db_bad = np.mean(db_bad, axis=0)
+#     else:
+#         # using raw data points
+#         out_g[count] = (int)(np.sum(g > (avg + std_dev_mult*std)))
 
-# # sum_db_good = np.sum(db_good, axis=0)
-# # sum_db_bad = np.sum(db_bad, axis=0)
+#     count += 1
+    
+#     fig.add_trace(go.Scatter(x=freq, y=g, name=f"good fan {count}",
+#                              line=dict(width=0.5)))
 
-# # fig.add_trace(go.Scatter(x=freq, y=avg_db_good, name="Average Good"))
-# # fig.add_trace(go.Scatter(x=freq, y=sum_db_good, name="Sum Good"))
-
-# # fig.add_trace(go.Scatter(x=freq, y=avg_db_bad, name="Average Bad"))
-# # fig.add_trace(go.Scatter(x=freq, y=sum_db_bad, name="Sum Bad"))
-
-# fig.update_layout(title="Peaks of the Signals", height=800, width=900,
-#                   xaxis_title="Frequency [Hz]", yaxis_title="Level [dB]")
+# if check_peak:
+#     fig.update_layout(title='fans with the avg / std dev using peaks', xaxis_title='Frequency(Hz)', yaxis_title='dB',
+#                   width=1000, height=700)
+# else:
+#     fig.update_layout(title='fans with the avg / std dev', xaxis_title='Frequency(Hz)', yaxis_title='dB',
+#                   width=1000, height=700)
+    
 # st.plotly_chart(fig)
 
+# col = st.columns(2)
+# with col[0]:
+#     st.header("bad fan outlier count:")
+#     st.table(out_b)
 
-# # st.dataframe(good_peaks_df, column_config=dict(width="medium"))
-# # st.dataframe(bad_peaks_df, column_config=dict(width="medium"))
+# with col[1]:
+#     st.header("good fan outlier count:")
+#     st.table(out_g)
+
+# #endregion
+    
 
 
-# st.data_editor(
-#     good_peaks_df,
-#     column_config={
-#         "widgets": st.column_config.Column(
-#             width="medium",
-#             disabled=True
-#         )
-#     },
-#     # hide_index=True,
-#     # num_rows="dynamic",
-# )
 
-# st.data_editor(
-#     bad_peaks_df,
-#     column_config={
-#         "widgets": st.column_config.Column(
-#             width="medium",
-#             disabled=True
-#         )
-#     },
-#     # hide_index=True,
-#     # num_rows="dynamic",
-# )
 
-# #endregion find peaks of signals
-
+""" USING ORIGINAL DATA WITHOUT SPRINGS
 
 #region compute average of good fans
 X = torch.load('X_tensor.pt')  
@@ -264,3 +233,4 @@ std = np.std(good, axis=0)
 # #endregion
     
 
+"""
