5017265f1b946ceffbc2403211e2a853c2ba7cea
joahkim-mill <joah.kim@chewielabs.com>
2024-03-08 12:05:44 -0800

    1100 hz

--- vibration_testing/accelerometer/collect_data.py
+++ vibration_testing/accelerometer/collect_data.py
@@ -240,8 +240,6 @@ class ADXL345:
 
         return val
 
-# If a user runs this file just display the latest values
-
 if __name__ == "__main__":
     accelerometer = ADXL345(0x53)
     
@@ -252,7 +250,8 @@ if __name__ == "__main__":
     accel_data = deque()
     print("Beginning data collection:")
     t0 = time.time()
-    duration = 3
+    duration = 5
+
     tf = t0 + duration
     while (time.time() < tf):
         axes = accelerometer.get_all_axes()
@@ -268,66 +267,6 @@ if __name__ == "__main__":
         # print("y: %.3f" % (axes['y']))
         # print("z: %.3f" % (axes['z']))
         
-
-"""
-# Get I2C bus
-bus = smbus.SMBus(1)
-
-# ADXL345 address, 0x53(83)
-# Select bandwidth rate register, 0x2C(44)
-#		0x0A(10)	Normal mode, Output data rate = 100 Hz
-bus.write_byte_data(0x53, 0x2C, 0x0A)
-# ADXL345 address, 0x53(83)
-# Select power control register, 0x2D(45)
-#		0x08(08)	Auto Sleep disable
-bus.write_byte_data(0x53, 0x2D, 0x08)
-# ADXL345 address, 0x53(83)
-# Select data format register, 0x31(49)
-#		0x08(08)	Self test disabled, 4-wire interface
-#					Full resolution, Range = +/-2g
-bus.write_byte_data(0x53, 0x31, 0x08)
-
-time.sleep(0.5)
-
-# ADXL345 address, 0x53(83)
-# Read data back from 0x32(50), 2 bytes
-# X-Axis LSB, X-Axis MSB
-data0 = bus.read_byte_data(0x53, 0x32)
-data1 = bus.read_byte_data(0x53, 0x33)
-
-# Convert the data to 10-bits
-xAccl = ((data1 & 0x03) * 256) + data0
-if xAccl > 511 :
-	xAccl -= 1024
-
-# ADXL345 address, 0x53(83)
-# Read data back from 0x34(52), 2 bytes
-# Y-Axis LSB, Y-Axis MSB
-data0 = bus.read_byte_data(0x53, 0x34)
-data1 = bus.read_byte_data(0x53, 0x35)
-
-# Convert the data to 10-bits
-yAccl = ((data1 & 0x03) * 256) + data0
-if yAccl > 511 :
-	yAccl -= 1024
-
-# ADXL345 address, 0x53(83)
-# Read data back from 0x36(54), 2 bytes
-# Z-Axis LSB, Z-Axis MSB
-data0 = bus.read_byte_data(0x53, 0x36)
-data1 = bus.read_byte_data(0x53, 0x37)
-
-# Convert the data to 10-bits
-zAccl = ((data1 & 0x03) * 256) + data0
-if zAccl > 511 :
-	zAccl -= 1024
-
-# Output data to screen
-print("Acceleration in X-Axis : %d" %xAccl)
-print ("Acceleration in Y-Axis : %d" %yAccl)
-print ("Acceleration in Z-Axis : %d" %zAccl)
-
-"""
 
 """
 ## make sure to change the name of the file it gets saved to, or it will get overwritten
--- vibration_testing/accelerometer/plot_accel.py
+++ vibration_testing/accelerometer/plot_accel.py
@@ -4,108 +4,80 @@ from plotly import graph_objects as go
 from plotly.subplots import make_subplots
 from scipy.fft import fft, fftfreq
 
-dataname = '68 with spring 45pwm.txt'
-data = pd.read_csv(f'./{dataname}', sep=',', skiprows=12)
-data.columns = ['time[ms]', 'x', 'y', 'z']
-# print(data)
+dataname = '28_100pwm_5s'
+data = pd.read_csv(f'./new_accel_data/{dataname}.csv',)
+# # data.columns = ['time', 'x_accel', 'y_accel', 'z_accel']
+# # print(data)
 
-# fig = go.Figure()
-fig = make_subplots(rows=3, cols=1,
-                    subplot_titles=("X", "Y", "Z"), 
-                    vertical_spacing=0.05
-                    )
+# # fig = go.Figure()
+# fig = make_subplots(rows=3, cols=1,
+#                     subplot_titles=("X", "Y", "Z"), 
+#                     vertical_spacing=0.05
+#                     )
 
-# acceleration in x
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['x'], name='x accel', line=dict(color="#aa81f0")),
-    row=1, col=1
-)
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['y'], name='y accel', line=dict(color="#aa81f0")),
-    row=2, col=1
-)
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['z'], name='z accel', line=dict(color="#aa81f0")),
-    row=3, col=1
-)
+# # acceleration in x
+# fig.add_trace(
+#     go.Scatter(x=data['time'], y=data['x_accel'], name='x accel', line=dict(color="#aa81f0")),
+#     row=1, col=1
+# )
+# fig.add_trace(
+#     go.Scatter(x=data['time'], y=data['y_accel'], name='y accel', line=dict(color="#aa81f0")),
+#     row=2, col=1
+# )
+# fig.add_trace(
+#     go.Scatter(x=data['time'], y=data['z_accel'], name='z accel', line=dict(color="#aa81f0")),
+#     row=3, col=1
+# )
 
-# fig.add_trace(go.Scatter())
-# fig.add_trace(go.Scatter(x=data['time[ms]'], y=data['y'], name='y accel'))
-# fig.add_trace(go.Scatter(x=data['time[ms]'], y=data['z'], name='z accel'))
+# dataname = '28'
+# data = pd.read_csv(f'./new_accel_data/{dataname}.csv',)
+# # acceleration in x
+# fig.add_trace(
+#     go.Scatter(x=data['time'], y=data['x_accel'], name='x accel', line=dict(color="#40b892")),
+#     row=1, col=1
+# )
+# fig.add_trace(
+#     go.Scatter(x=data['time'], y=data['y_accel'], name='y accel', line=dict(color="#40b892")),
+#     row=2, col=1
+# )
+# fig.add_trace(
+#     go.Scatter(x=data['time'], y=data['z_accel'], name='z accel', line=dict(color="#40b892")),
+#     row=3, col=1
+# )
 
-data2 = 'bad fan test.txt'
-data = pd.read_csv(f'./{data2}', sep=',', skiprows=12)
-data.columns = ['time[ms]', 'x', 'y', 'z']
-# print(data)
-
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['x'], name='bad fan x accel', line=dict(color="#41adf0")),
-    row=1, col=1
-)
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['y'], name='bad fan y accel', line=dict(color="#41adf0")),
-    row=2, col=1
-)
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['z'], name='bad fan z accel', line=dict(color="#41adf0")),
-    row=3, col=1
-)
-
-data3 = '28 with spring 45pwm.txt'
-data = pd.read_csv(f'./{data3}', sep=',', skiprows=12)
-data.columns = ['time[ms]', 'x', 'y', 'z']
-# print(data)
-
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['x'], name='28 x accel', line=dict(color="#40b892")),
-    row=1, col=1
-)
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['y'], name='28 y accel', line=dict(color="#40b892")),
-    row=2, col=1
-)
-fig.add_trace(
-    go.Scatter(x=data['time[ms]'], y=data['z'], name='28 z accel', line=dict(color="#40b892")),
-    row=3, col=1
-)
-
-# fig.add_trace(go.Scatter(x=data['time[ms]'], y=data['x'], name='bad fan x accel'))
-# fig.add_trace(go.Scatter(x=data['time[ms]'], y=data['y'], name='bad fan y accel'))
-# fig.add_trace(go.Scatter(x=data['time[ms]'], y=data['z'], name='bad fan z accel'))
-
-fig.show()
-
-
-# fig = go.Figure()
-
-# t = np.asarray(data["time[ms]"])
-# ax = np.asarray(data["x"])
-# ay = np.asarray(data["y"])
-# az = np.asarray(data["z"])
-# a = np.hstack((ax.reshape((-1,1)), ay.reshape((-1,1)), az.reshape((-1,1))))  # [ax , ay, az] columnwise
-
-# N = len(t)
-# sample_rate = 333 # Hz 
-# T = 1/sample_rate 
-# yf_x = fft(ax)
-# xf_x = fftfreq(N, T)[:N//2]
-# y_x = 2.0/N * abs(yf_x[0:N//2])
-# fig.add_trace(go.Scatter(x=xf_x, y=y_x, line_shape='linear', name=f"ax")) 
-
-# yf_y = fft(ay)
-# xf_y = fftfreq(N, T)[:N//2]
-# y_y = 2.0/N * abs(yf_y[0:N//2])
-# fig.add_trace(go.Scatter(x=xf_y, y=y_y, line_shape='linear', name=f"ay")) 
-
-# yf_z = fft(az)
-# xf_z = fftfreq(N, T)[:N//2]
-# y_z = 2.0/N * abs(yf_z[0:N//2])
-# fig.add_trace(go.Scatter(x=xf_z, y=y_z, line_shape='linear', name=f"az")) 
-
-# fig.update_layout(title=f"Fan FFT of {dataname}", 
-#                 xaxis_title="Frequency [Hz]", 
-#               #   yaxis_title="SPL [dB]",
-#                 height=600,
-#                 width=800)
-# fig.update_yaxes(range=[0, 1])
 # fig.show()
+
+
+fig = go.Figure()
+
+t = np.asarray(data["time"])
+ax = np.asarray(data["x_accel"])
+ay = np.asarray(data["y_accel"])
+az = np.asarray(data["z_accel"])
+a = np.hstack((ax.reshape((-1,1)), ay.reshape((-1,1)), az.reshape((-1,1))))  # [ax , ay, az] columnwise
+
+N = len(t)
+sample_rate = 1100 # Hz 
+T = 1/sample_rate 
+yf_x = fft(ax)
+xf_x = fftfreq(N, T)[:N//2]
+y_x = 2.0/N * abs(yf_x[0:N//2])
+fig.add_trace(go.Scatter(x=xf_x, y=y_x, line_shape='linear', name=f"ax")) 
+
+yf_y = fft(ay)
+xf_y = fftfreq(N, T)[:N//2]
+y_y = 2.0/N * abs(yf_y[0:N//2])
+fig.add_trace(go.Scatter(x=xf_y, y=y_y, line_shape='linear', name=f"ay")) 
+
+yf_z = fft(az)
+xf_z = fftfreq(N, T)[:N//2]
+y_z = 2.0/N * abs(yf_z[0:N//2])
+fig.add_trace(go.Scatter(x=xf_z, y=y_z, line_shape='linear', name=f"az")) 
+
+fig.update_layout(title=f"Fan FFT of {dataname}", 
+                xaxis_title="Frequency [Hz]", 
+              #   yaxis_title="SPL [dB]",
+                height=600,
+                width=800)
+fig.update_yaxes(range=[0, 1])
+fig.show()
